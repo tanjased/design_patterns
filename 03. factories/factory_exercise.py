6c2ca9c3be5d7c52bc4811f49eb810953989a5fc
tanjased <71439082+tanjased@users.noreply.github.com>
2021-03-13 15:52:41 +0100

    commit

--- 03. factories/factory_exercise.py
+++ 03. factories/factory_exercise.py
@@ -1,8 +1,22 @@
+import itertools
+
+
 class Person:
+
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
+    def __str__(self):
+        return f'Person: {self.id}: {self.name}'
+
 class PersonFactory:
+    id = itertools.count()
+
     def create_person(self, name):
-        pass
+        p = Person(next(PersonFactory.id), name)
+        return p
+
+p1 = PersonFactory.create_person('y','Celine')
+p2 = PersonFactory.create_person(0,'Celine')
+print(p1,'\n',p2)
